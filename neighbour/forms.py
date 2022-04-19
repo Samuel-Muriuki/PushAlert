aa12dbe5e9761e8ee70e04c474af08b33f319d8c
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:46:04 +0300

    Creating the hood form

--- neighbour/forms.py
+++ neighbour/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from . models import NeighbourHood,Post,Business,Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -11,3 +12,8 @@ class UserRegisterForm(UserCreationForm):
         fields= ['username','email','password1','password2']
         
 User._meta.get_field('email')._unique=True
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)
