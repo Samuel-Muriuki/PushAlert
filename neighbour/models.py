d5f105870c42faeae8261b1f99ea730445862e2f
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:41:20 +0300

    Creating the profile model

--- neighbour/models.py
+++ neighbour/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from phone_field import PhoneField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class NeighbourHood(models.Model):
@@ -35,3 +37,23 @@ class NeighbourHood(models.Model):
     @classmethod
     def find_neighborhood(cls, neighborhood_id):
         return cls.objects.filter(id=neighborhood_id)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=400, blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    profile_pic = CloudinaryField('image', default='https://res.cloudinary.com/samm-gallery/image/upload/v1649751098/Ninja_fjh4xm.png')
+    phone_number = PhoneField(max_length=15, blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
