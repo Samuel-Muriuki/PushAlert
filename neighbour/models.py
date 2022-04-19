acb458297deba53d6644e4949d83b33c5f20d4e6
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:39:55 +0300

    Creating the neighbourhood model

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from phone_field import PhoneField
 
 # Create your models here.
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hood", null=True)
+    hood_logo = CloudinaryField('image')
+    description = models.TextField()
+    health_tell = PhoneField(null=True, blank=True)
+    police_number = PhoneField(null=True, blank=True)
+    area_administrator = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        ordering = ['-pk']
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    def update_neighborhood(self):
+        self.update()
+
+    def update_occupants(self):
+        self.update()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
