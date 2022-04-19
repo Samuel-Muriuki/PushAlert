10ede5f3d5c95d651d017e25163ce784eaba731d
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:54:16 +0300

    Defining the hoods page

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect, render
 from . forms import UpdateUserForm, UpdateUserProfileForm, UserRegisterForm, HoodForm, BusinessForm, PostForm
+from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood, Business, Post, Profile
 
 # Create your views here.
 def index(request):
@@ -21,4 +23,10 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, "registration/register.html", {'form': form})
+
+@login_required(login_url='login')
+def hoods(request):
+    current_user = request.user
+    hoods = NeighbourHood.objects.all()
+    return render(request, "all-neighbour/hoods.html", {'hoods': hoods, 'current_user': current_user})
 
