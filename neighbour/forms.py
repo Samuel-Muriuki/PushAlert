bfde72558c99c53558e9f4051062a8f5de024586
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:46:32 +0300

    Creating the user update form

--- neighbour/forms.py
+++ neighbour/forms.py
@@ -17,3 +17,10 @@ class HoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
         exclude = ('admin',)
+        
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
