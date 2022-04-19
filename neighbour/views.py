5ce9d5c2600a3081c9592978c44a6c882b1316e2
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:56:10 +0300

    Defining the new-hood page

--- neighbour/views.py
+++ neighbour/views.py
@@ -4,6 +4,7 @@ from . forms import UpdateUserForm, UpdateUserProfileForm, UserRegisterForm, Hoo
 from django.contrib.auth.decorators import login_required
 from .models import NeighbourHood, Business, Post, Profile
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -48,4 +49,17 @@ def profile(request, username):
         profile_form = UpdateUserProfileForm(instance=request.user.profile)
 
     return render(request, 'all-neighbour/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+
+@login_required(login_url='login')
+def new_hood(request):
+    if request.method == 'POST':
+        form = HoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user
+            hood.save()
+            return HttpResponseRedirect(reverse("hoods"))
+    else:
+        form = HoodForm()
+    return render(request, 'all-neighbour/newhood.html', {'form': form})
 
