b64316aed8201709f170fdd21f6c58697f39c423
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:57:59 +0300

    Defining the new post page

--- neighbour/views.py
+++ neighbour/views.py
@@ -98,3 +98,19 @@ def new_business(request, id):
         bus_form = BusinessForm()
     return render(request, 'all-neighbour/business.html', {'bus_form': bus_form, 'title': title})
 
+@login_required(login_url='login')
+def new_post(request, id):
+    hood = NeighbourHood.objects.get(id=id)
+    title = 'ADD POST'
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.neighbourhood = hood
+            post.owner = request.user
+            post.save()
+            return redirect('my_hood', id)
+    else:
+        post_form = PostForm()
+    return render(request, 'all-neighbour/post.html', {'post_form': post_form, 'title': title})
+
