b9e0aed4cbca5f7755249503e79d8dc50230c02a
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-19 04:58:51 +0300

    Defining thebusiness update page

--- neighbour/views.py
+++ neighbour/views.py
@@ -114,3 +114,16 @@ def new_post(request, id):
         post_form = PostForm()
     return render(request, 'all-neighbour/post.html', {'post_form': post_form, 'title': title})
 
+@login_required(login_url='login')
+def update_business(request, id, bus_id):
+    instance = Business.objects.get(id=bus_id)
+    title = 'UPDATE BUSINESS'
+    if request.method == 'POST':
+        bus_form = BusinessForm(request.POST, request.FILES, instance=instance)
+        if bus_form.is_valid():
+            bus_form.save()
+        return redirect('my_hood', id)
+    else:
+        bus_form = BusinessForm(instance=instance)
+    return render(request, 'all-neighbour/business.html', {'bus_form': bus_form, 'title': title})
+
